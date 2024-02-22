34196111eb6602aea82e6c6933d6b9ee4067110d
Kemily Cristina Schmidt de Oliveira <kemily.c2006@aluno.ifsc.edu.br>
2024-02-22 16:03:32 -0300

    Classes Evento, Hotel e ModeloQuarto e suas interfaces

---
+++ src/main/java/controle/HotelDAO.java
@@ -0,0 +1,34 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.hotel;
+
+public class HotelDAO implements IHotelDAO {
+
+	public int inserirHotel(hotel end) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ArrayList<hotel> listarHotel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean atualizarHotel(hotel end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean removerHotel(hotel end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public hotel buscarHotelPorEndereco(String endereco) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
