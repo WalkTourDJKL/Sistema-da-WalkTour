a320291393a5d04669da116dcc07efac5eaf26e0
José Paulo Baptista <jose.p2005@aluno.ifsc.edu.br>
2024-02-22 15:27:17 -0300

    Classe EnderecoDAO e sua interface

---
+++ src/main/java/controle/EnderecoDAO.java
@@ -0,0 +1,34 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.endereco;
+
+public class EnderecoDAO implements IEnderecoDAO{
+
+	public int inserirEndereco(endereco end) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ArrayList<endereco> listarEndereco() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean atualizarEndereco(endereco end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean removerEndereco(endereco end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public endereco buscarEnderecoPorCEP(int cpf) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
