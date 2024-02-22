0aa634c55af94db49cf0db89bd322c10f98ae685
Lucas Guerega <lucas.g2006@aluno.ifsc.edu.br>
2024-02-22 13:04:48 -0300

    Classe ClienteDAO e a sua interface

--- src/main/java/controle/ClienteDAO.java
+++ src/main/java/controle/ClienteDAO.java
@@ -1,5 +1,34 @@
 package controle;
 
-public class ClienteDAO {
+import java.util.ArrayList;
+
+import modelo.cliente;
+
+public class ClienteDAO implements IClienteDAO {
+	
+	public int inserirCliente(cliente end) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ArrayList<cliente> listarClientes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean atualizarCliente(cliente end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean removerCliente(cliente end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public cliente buscarClientePorCPF(int cpf) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
