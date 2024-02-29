9cb78cefe8f432aa442bafe974202bd3e3b48735
José Paulo Baptista <jose.p2005@aluno.ifsc.edu.br>
2024-02-29 14:00:08 -0300

    Endereco connection

--- src/main/java/controle/EnderecoDAO.java
+++ src/main/java/controle/EnderecoDAO.java
@@ -1,33 +1,83 @@
 package controle;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import modelo.endereco;
 
 public class EnderecoDAO implements IEnderecoDAO{
+	
+	private static EnderecoDAO instancia;
+	
+	private EnderecoDAO() {
+		
+	}
+	
+	public static EnderecoDAO getInstancia() {
+		if (instancia == null) {
+			instancia = new EnderecoDAO();
+		}
+		return instancia;
+	}
+	
+	
 
 	public int inserirEndereco(endereco end) {
-		// TODO Auto-generated method stub
+		String SQL = "INSERT INTO endereco (endereco_id,cep,cidade,estado,pais) VALUES (?,?,?,?,?)";
 		return 0;
 	}
 
 	public ArrayList<endereco> listarEndereco() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<endereco> enderecos = new ArrayList<endereco>();
+		
+		String SQL = "SELECT * FROM endereco";
+		
+		Conexao con = Conexao.getConexao(); //Pegou a conexão do banco de dados
+		Connection conDB = con.conectar();  //Se conectou realmente ao banco de dados
+		try {
+			PreparedStatement ps = conDB.prepareStatement(SQL);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				endereco end = new endereco();
+				
+				Integer enderecoId = rs.getInt("endereco_id");
+				String cep = rs.getString("cep");
+				String cidade = rs.getString("cidade");
+				String estado = rs.getString("estado");
+				String pais = rs.getString("pais");
+				
+				end.setEnderecoId(enderecoId);
+				end.setCep(cep);
+				end.setCidade(cidade);
+				end.setEstado(estado);
+				end.setPais(pais);
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			con.fecharConexao();
+		}
+		
+		return enderecos;
 	}
 
 	public boolean atualizarEndereco(endereco end) {
-		// TODO Auto-generated method stub
+		String SQL = "UPDATE endereco SET cep = ?, cidade = ?, estado = ?, pais = ? WHERE endereco_id = ?";
 		return false;
 	}
 
 	public boolean removerEndereco(endereco end) {
-		// TODO Auto-generated method stub
+		String SQL = "DELETE FROM endereco WHERE endereco_id = ?";
 		return false;
 	}
 
 	public endereco buscarEnderecoPorCEP(int cpf) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
