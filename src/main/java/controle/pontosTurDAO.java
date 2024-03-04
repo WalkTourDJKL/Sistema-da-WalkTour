42f30fa3dd660eb8c881ce5de3ee1ca1016d327d
Douglas <douglas.ef@aluno.ifsc.edu.br>
2024-03-04 10:52:46 -0300

    Auto stash before merge of "classesControle" and "origin/classesControle"
    Insert e Select do pontosTur

--- src/main/java/controle/pontosTurDAO.java
+++ src/main/java/controle/pontosTurDAO.java
@@ -1,20 +1,95 @@
 package controle;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
 import java.util.ArrayList;
 
 import modelo.pontosTur;
 
 public class pontosTurDAO implements IpontosTurDAO {
+	
+	private static pontosTurDAO instancia;
+	
+	private pontosTurDAO() {
+		
+	}
+	
+	public static pontosTurDAO getInstancia() {
+		if(instancia == null) {
+			instancia = new pontosTurDAO();
+		}
+		return instancia;
+	}
 
 	public int inserirPontoTur(pontosTur end) {
-		// TODO Auto-generated method stub
-		return 0;
+String SQL = "INSERT INTO pontosTur(ponto_id,horaabre,horafecha,preco,nomepontotur) VALUES (?,?,?,?,?)";
+		
+		Conexao con = Conexao.getConexao();
+		Connection conDB = con.conectar();
+		
+		
+		try {
+			PreparedStatement ps = conDB.prepareStatement(SQL);
+		
+			ps.setInt(1, end.getPontoId());
+			ps.setTime(2, end.getHoraAbre());
+			ps.setTime(3, end.getHoraFecha());
+			ps.setFloat(4, end.getPreco());
+			ps.setString(5, end.getNomePontoTur());			
+			
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			con.fecharConexao();
+		}
+		return 0;	
+		
+		
 	}
 
 	public ArrayList<pontosTur> listarPontoTur() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+			ArrayList<pontosTur> pontosTur = new ArrayList<pontosTur>();
+			
+			String SQL = "SELECT * FROM pontosTur";
+			
+			Conexao con = Conexao.getConexao();
+			Connection conDB = con.conectar();
+			
+			try {
+				PreparedStatement ps = conDB.prepareStatement(SQL);
+				
+				ResultSet rs = ps.executeQuery();
+				
+				while (rs.next()) {
+					pontosTur end = new pontosTur();
+					
+					Integer pontoId = rs.getInt("ponto_id");
+					Time horaAbre = rs.getTime("horaabre");
+					Time horaFecha = rs.getTime("horafecha");
+					Float preco = rs.getFloat("preco");
+					String nomePontoTur = rs.getString("nomepontotur");
+					
+					end.setPontoId(pontoId);
+					end.setHoraAbre(horaAbre);
+					end.setHoraFecha(horaFecha);
+					end.setPreco(preco);
+					end.setNomePontoTur(nomePontoTur);
+				
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				con.fecharConexao();
+				
+			}
+			return pontosTur;
+		
+		}
 
 	public boolean atualizarPontosTur(pontosTur end) {
 		// TODO Auto-generated method stub
@@ -29,6 +104,10 @@ public class pontosTurDAO implements IpontosTurDAO {
 	public pontosTur buscarPontoTurPorNome(String nomePontoTur) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public static void setInstancia(pontosTurDAO instancia) {
+		pontosTurDAO.instancia = instancia;
 	}
 
 }
