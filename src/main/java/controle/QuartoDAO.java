c10845b94576c01b24609e15dd8c07d36178be61
Douglas <douglas.ef@aluno.ifsc.edu.br>
2024-03-04 11:24:42 -0300

    Insert e Select do quarto

--- src/main/java/controle/QuartoDAO.java
+++ src/main/java/controle/QuartoDAO.java
@@ -1,19 +1,92 @@
 package controle;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
 import java.util.ArrayList;
 
 import modelo.quarto;
 
 public class QuartoDAO implements IQuartoDAO{
+	
+	private static QuartoDAO instancia;
+	
+	private QuartoDAO() {
+		
+	}
+	
+	public static QuartoDAO getInstancia1() {
+		if(instancia == null) {
+			instancia = new QuartoDAO();
+		}
+		return instancia; 
+	}
 
 	public int inserirQuarto(quarto end) {
-		// TODO Auto-generated method stub
-		return 0;
+String SQL = "INSERT INTO quarto(numquarto,horalimpeza,servicoquarto,tipo_Id) VALUES (?,?,?,?)";
+		
+		Conexao con = Conexao.getConexao();
+		Connection conDB = con.conectar();
+		
+		
+		try {
+			PreparedStatement ps = conDB.prepareStatement(SQL);
+		
+			ps.setInt(1, end.getNumQuarto());
+			ps.setTime(2, end.getHoraLimpeza());
+			ps.setBoolean(3, end.isServicoQuarto());
+			ps.setInt(4, end.getTipoId());
+			
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			con.fecharConexao();
+		}
+		return 0;	
+		
+		
 	}
 
 	public ArrayList<quarto> listarQuartos() {
-		// TODO Auto-generated method stub
-		return null;
+ArrayList<quarto> quarto = new ArrayList<quarto>();
+		
+		String SQL = "SELECT * FROM quarto";
+		
+		Conexao con = Conexao.getConexao();
+		Connection conDB = con.conectar();
+		
+		try {
+			PreparedStatement ps = conDB.prepareStatement(SQL);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				quarto end = new quarto();
+				
+				Integer numQuarto = rs.getInt("numquarto");
+				Time horaLimpeza = rs.getTime("horalimpeza");
+				Boolean servicoQuarto = rs.getBoolean("servicoquarto");
+				Integer tipoId = rs.getInt("tipo_id");
+				
+				
+				end.setNumQuarto(numQuarto);
+				end.setHoraLimpeza(horaLimpeza);
+				end.setServicoQuarto(servicoQuarto);
+				end.setTipoId(tipoId);
+
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			con.fecharConexao();
+			
+		}
+		return quarto;
+	
 	}
 
 	public boolean atualizarQuarto(quarto end) {
@@ -29,6 +102,14 @@ public class QuartoDAO implements IQuartoDAO{
 	public quarto buscarQuartoPorNúmero(int numQuarto) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public static QuartoDAO getInstancia() {
+		return instancia;
+	}
+
+	public static void setInstancia(QuartoDAO instancia) {
+		QuartoDAO.instancia = instancia;
 	}
 
 }
