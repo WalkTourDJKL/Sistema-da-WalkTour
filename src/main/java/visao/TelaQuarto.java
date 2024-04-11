549bf8993c11ca75be067e0fe44ad689606cf1de
Lucas Guerega <lucas.g2006@aluno.ifsc.edu.br>
2024-04-10 21:50:45 -0300

    Pequenos ajustes na tela quarto

--- src/main/java/visao/TelaQuarto.java
+++ src/main/java/visao/TelaQuarto.java
@@ -30,12 +30,12 @@ public class TelaQuarto extends JFrame {
         contentPane.setLayout(null);
         
         JLabel lblTitulo = new JLabel("");
-        lblTitulo.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\target\\classes\\imgs\\titulo.png"));
+        lblTitulo.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/titulo.png")));
         lblTitulo.setBounds(10, 11, 1063, 250);
         contentPane.add(lblTitulo);
         
         JLabel lblIcon = new JLabel("");
-        lblIcon.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\target\\classes\\imgs\\perfil.png"));
+        lblIcon.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/perfil.png")));
         lblIcon.setBounds(1772, 35, 99, 95);
         contentPane.add(lblIcon);
         
@@ -45,7 +45,7 @@ public class TelaQuarto extends JFrame {
         contentPane.add(lblNewLabel);
         
         JLabel lblImgQuarto = new JLabel("");
-        lblImgQuarto.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\src\\main\\java\\imgs\\quarto.png"));
+        lblImgQuarto.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/quarto.png")));
         lblImgQuarto.setBounds(120, 294, 654, 545);
         contentPane.add(lblImgQuarto);
         
@@ -59,45 +59,45 @@ public class TelaQuarto extends JFrame {
         contentPane.add(lblNomeHotel);
         
         JLabel lblWifi = new JLabel("");
-        lblWifi.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\src\\main\\java\\imgs\\WifiIcon.png"));
+        lblWifi.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/WifiIcon.png")));
         lblWifi.setFont(new Font("Tahoma", Font.PLAIN, 17));
         lblWifi.setBounds(1136, 498, 65, 32);
         contentPane.add(lblWifi);
         
         JLabel lblChave = new JLabel("");
-        lblChave.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\src\\main\\java\\imgs\\Key.png"));
+        lblChave.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/Key.png")));
         lblChave.setFont(new Font("Tahoma", Font.PLAIN, 17));
         lblChave.setBounds(1300, 498, 98, 45);
         contentPane.add(lblChave);
         
         JLabel lblArCon = new JLabel("");
-        lblArCon.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\src\\main\\java\\imgs\\ArCondicionado.png"));
+        lblArCon.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/ArCondicionado.png")));
         lblArCon.setFont(new Font("Tahoma", Font.PLAIN, 17));
         lblArCon.setBounds(1498, 498, 98, 68);
         contentPane.add(lblArCon);
         
         JLabel lblMaquinaCafe = new JLabel("");
-        lblMaquinaCafe.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\src\\main\\java\\imgs\\MaquinaCafe.png"));
+        lblMaquinaCafe.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/MaquinaCafe.png")));
         lblMaquinaCafe.setFont(new Font("Tahoma", Font.PLAIN, 17));
         lblMaquinaCafe.setBounds(1698, 478, 78, 72);
         contentPane.add(lblMaquinaCafe);
         
         JLabel lblCafe = new JLabel("");
-        lblCafe.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\src\\main\\java\\imgs\\cafeIcon.png"));
+        lblCafe.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/cafeIcon.png")));
         lblCafe.setBounds(1136, 647, 65, 78);
         contentPane.add(lblCafe);
         
         JLabel lblTv = new JLabel("");
-        lblTv.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\src\\main\\java\\imgs\\Tv.png"));
+        lblTv.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/Tv.png")));
         lblTv.setBounds(1300, 658, 78, 72);
         contentPane.add(lblTv);
         
         JLabel lblComputador = new JLabel("");
-        lblComputador.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Sistema-da-WalkTour\\src\\main\\java\\imgs\\PcDisplay.png"));
+        lblComputador.setIcon(new ImageIcon(TelaQuarto.class.getResource("/imgs/PcDisplay.png")));
         lblComputador.setBounds(1515, 645, 76, 89);
         contentPane.add(lblComputador);
         
-        JLabel lblWifiGra = new JLabel("Wifi Grátis ");
+        JLabel lblWifiGra = new JLabel("Wifi Gr�tis ");
         lblWifiGra.setFont(new Font("Arial", Font.PLAIN, 14));
         lblWifiGra.setBounds(1128, 577, 78, 14);
         contentPane.add(lblWifiGra);
@@ -112,9 +112,9 @@ public class TelaQuarto extends JFrame {
         lblArcondi.setBounds(1500, 577, 99, 25);
         contentPane.add(lblArcondi);
         
-        JLabel lblCafé = new JLabel("Máquina de Café ");
-        lblCafé.setFont(new Font("Arial", Font.PLAIN, 14));
-        lblCafé.setBounds(1685, 583, 115, 14);
-        contentPane.add(lblCafé);
+        JLabel lblCafeMaq = new JLabel("M�quina de Caf�");
+        lblCafeMaq.setFont(new Font("Arial", Font.PLAIN, 14));
+        lblCafeMaq.setBounds(1685, 583, 115, 14);
+        contentPane.add(lblCafeMaq);
     }
 }
